1b3fa158a7800c63899cd6ed67a809ba710f6f5c
gjdame <gregoryjdame@gmail.com>
2019-03-03 23:03:47 +0000

    comments

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+"""Python function to find dimension of matrix"""
 
 def matrix_shape(matrix):
+    """Python function to find dimension of matrix"""
     import numpy as np
     arr = np.array(matrix)
     return (list(arr.shape))
-    
