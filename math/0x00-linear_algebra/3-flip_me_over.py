1edc31299abcd11c3b4a28ee3d5226c31bcd3fbc
gjdame <gregoryjdame@gmail.com>
2019-03-04 00:51:10 +0000

    add 3

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""transpose without numpy"""
+
+
+def matrix_transpose(matrix):
+    """transpose without numpy"""
+    res = []
+    for i in range(len(matrix[0])):
+        r = []
+        for j in range(len(matrix[i])):
+            r.append(matrix[j][i])
+        res.append(r)
+    return(res)
