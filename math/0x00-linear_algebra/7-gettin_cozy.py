c7a76683b77268a2ceb19df7a5bc3e6264a84d50
gjdame <gregoryjdame@gmail.com>
2019-03-05 04:16:57 +0000

    try 7 again

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-
+"""add matricies"""
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """add matricies"""
-
-    res = []
-    if axis == 1:
-        res.append(mat1[0] + mat2[0])
-        res.append(mat1[1] + mat2[1])
+    if axis == 0 and len(mat1[0]) != len(mat2[0]):
+        return None
+    if axis == 1 and len(mat1) != len(mat2):
+        return None
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        res = []
+        res.extend(mat1)
+        res.extend(mat2)
         return(res)
-    else:
-        res.append(mat1)
-        res.append(mat2[0])
+    if axis == 1 and len(mat1) == len(mat2):
+        res = []
+        for i in range(len(mat1)):
+            r = []
+            r.extend(mat1[i])
+            r.extend(mat2[i])
+            res.extend([r])
         return(res)
 
