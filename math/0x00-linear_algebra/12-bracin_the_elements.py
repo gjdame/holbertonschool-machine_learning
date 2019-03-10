95a610e4f96a274a19abfc1171e6580348d8f0b5
gjdame <gregoryjdame@gmail.com>
2019-03-10 18:49:31 +0000

    pycodestyle

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -5,4 +5,3 @@
 def np_elementwise(mat1, mat2):
     c = ((mat1 + mat2), (mat1 - mat2), (mat1 * mat2), (mat1 / mat2))
     return (c)
-        
--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+''' concat matricies '''
 import numpy as np
 
 
