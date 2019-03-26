3219eb1840f2b5937aafb40d717d30ddeb201497
Gregory Dame <gdame@atlassian.com>
2019-03-25 19:13:22 -0700

    pycodestyle

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''basic sigma notation in python'''
 
+
 def summation_i_squared(n):
-	'''basic sigma noation in pyhthon'''
-	if n < i:
-		return None
-	my_list = list(range(1, n))
-	my_list2 = [x ** 2 for x in my_list]
-	return(sum(x for x in my_list2))
+    '''basic sigma noation in pyhthon'''
+    if n < i:
+        return None
+    my_list = list(range(1, n))
+    my_list2 = [x ** 2 for x in my_list]
+    return(sum(x for x in my_list2))
